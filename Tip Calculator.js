1afb1594bb707521c58f49b1221afd5f5eb5f163
Mandeep Sandhu <mandeepsan14@gmail.com>
2020-07-02 14:11:50 -0700

    JS Done

--- Tip Calculator.js
+++ Tip Calculator.js
@@ -1,4 +1,4 @@
-// tip calculator
+//tip calculator
 
 function calculateTip() {
   var billAmt = document.getElementById("billamt").value;
@@ -9,7 +9,7 @@ function calculateTip() {
     alert("please enter values");
     return;
   }
-  // see if this input is empty or less than or equal to 1
+  //see if this input is empty or less than or equal to 1
 
   if (numOfPeople === "" || numOfPeople <= 1) {
     numOfPeople = 1;
@@ -17,5 +17,29 @@ function calculateTip() {
   } else {
     document.getElementById("each").style.display = "block";
   }
-  
+  //calculation
+var total = (billAmt * serviceQual) / numOfPeople;
+//round two decimal points
+
+total = Math.round(total*100) / 100;
+
+// two digits after decimal point
+
+total = total.toFixed(2);
+
+//display tip amount
+
+document.getElementById("totalTip").style.display = "block";
+document.getElementById("tip").innerHTML = total;
+
 }
+// hide the tip amount on load
+
+document.getElementById("totalTip").style.display = "none";
+document.getElementById("each").style.display = "none";
+
+//click function
+
+document.getElementById("calculate").onclick = function(){
+  calculateTip();
+};
